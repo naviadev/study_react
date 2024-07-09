6a6c9560770e5ecc5c90ea4082c0c6dd4a4bb149
naviadev <qo2oq1020@gmail.com>
2024-07-09 11:46:01 +0900

    Feat | index.tsx : app.tsx를 reactDOM에 렌더

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,7 @@
-const a = 1;
-console.log(1)
+import App from "./app";
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(<App/>);
